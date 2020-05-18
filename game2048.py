dacb8382206d357443422ef9d97a4a8f81f208e3
pebblestack <tusharchakraborty99@gmail.com>
2020-05-19 00:35:41 +0530

    made changes in code for error handling

--- game2048.py
+++ game2048.py
@@ -4,7 +4,7 @@ import numpy as np
 import time
 import argparse
 import pynput
-from pynput.keyboard import Key, Listener
+from pynput.keyboard import Key, Listener, Controller
 
 def game_board(game_size):
     game = np.zeros((game_size,game_size), dtype = np.int)
@@ -12,10 +12,14 @@ def game_board(game_size):
 
 def show_game(current_game):
     for row in current_game:
+        print('\t', end = '  ')
         for item in row:
-            print(item, end='    ')
+            if item != 0:
+                print(item, end='    ')
+            else:
+                print('-', end='    ')
         print("\n")
-    print('*****'*len(current_game), end='    ')
+    print('\t', '*****'*len(current_game), end='    ')
     print(f"Winning tile: {win_tile}", end='\n')
 
 def swipe_up(current_game, store_game):
@@ -157,6 +161,12 @@ def check_win(current_game):
         elif count2 == 0:
             return False, False
 
+def press_esc():
+    keyboard = Controller()
+    
+    keyboard.press(Key.esc)
+    keyboard.release(Key.esc)
+
 os.system("cls")
 
 try:
@@ -201,71 +211,76 @@ try:
     def on_press(key):
         global num_of_moves, start, game, play, game_over, you_win, store_game
         try:
+            _, you_win = check_win(game)
+
             move = key.char
                     
-
-            os.system('cls')
+            if not you_win:
+                os.system('cls')
                     
-                        
-            if move == 'w':
-                num_of_moves += 1
-                invalid_move = False
-                game, store_game = swipe_up(game, store_game)
+                if move == 'w':
+                    num_of_moves += 1
+                    invalid_move = False
+                    game, store_game = swipe_up(game, store_game)
+                    
+                    
+                elif move == 's':
+                    num_of_moves += 1
+                    invalid_move = False
+                    game, store_game = swipe_down(game, store_game)
+                    
+                    
+                elif move == 'a':
+                    num_of_moves += 1
+                    invalid_move = False
+                    game, store_game = swipe_left(game, store_game)
+                    
+                    
+                elif move == 'd':
+                    num_of_moves += 1
+                    invalid_move = False
+                    game, store_game = swipe_right(game, store_game)
+                    
                 
-                
-            elif move == 's':
-                num_of_moves += 1
-                invalid_move = False
-                game, store_game = swipe_down(game, store_game)
-                
-                
-            elif move == 'a':
-                num_of_moves += 1
-                invalid_move = False
-                game, store_game = swipe_left(game, store_game)
-                
-                
-            elif move == 'd':
-                num_of_moves += 1
-                invalid_move = False
-                game, store_game = swipe_right(game, store_game)
-                
-            
-            elif move == 'u':
-                game = store_game
-                invalid_move = False
-                
-                
-            else:
-                print("Invalid move, try again!")
-                invalid_move = True
+                elif move == 'u':
+                    game = store_game
+                    invalid_move = False
+                    
+                    
+                else:
+                    print("Invalid move, try again!")
+                    invalid_move = True
 
-            comparison = game == store_game
-            equal_arrays = comparison.all() 
+                comparison = game == store_game
+                equal_arrays = comparison.all() 
 
-            if invalid_move:
-                game = store_game
+                if invalid_move:
+                    game = store_game
 
-            if equal_arrays and not invalid_move and move != 'u':
-                print("Warning!! Move in that direction not possible, try again!")
+                if equal_arrays and not invalid_move and move != 'u':
+                    print("Warning!! Move in that direction not possible, try again!")
 
-            if move != 'u' and not invalid_move and not equal_arrays:
-                get = True
-                while get:
-                    rand_row = random.randint(0,len(game)-1)
-                    rand_col = random.randint(0,len(game)-1)
-                    num = game[rand_row][rand_col]
-                    if num == 0:
-                        game[rand_row][rand_col] = 2
-                        get = False
+                if move != 'u' and not invalid_move and not equal_arrays:
+                    get = True
+                    while get:
+                        rand_row = random.randint(0,len(game)-1)
+                        rand_col = random.randint(0,len(game)-1)
+                        num = game[rand_row][rand_col]
+                        if num == 0:
+                            game[rand_row][rand_col] = 2
+                            get = False
 
         except AttributeError:
             print('')
 
     def on_release(key):
+        global you_win
+        
+
         if key == Key.esc:
             return False
-        show_game(game)
+        if not you_win:
+            show_game(game)
 
         game_over, you_win = check_win(game)
         if game_over and not you_win:
@@ -273,20 +288,21 @@ try:
             if not args.quiet:
                 print("Total number of moves made: ", num_of_moves)
                 print(f"Total time taken is {int(time.time() - start)} seconds")
-            print("You Loose!")
+            print("Sorry, You Loose")
+            press_esc()
         elif game_over and you_win:
             print('')
             if not args.quiet:
                 print("Total number of moves made: ", num_of_moves)
                 print(f"Total time taken is {int(time.time() - start)} seconds")
-            print("You Win!")
+            print("Congratulations, You Win")
+            press_esc()
         
 
     with Listener(on_press=on_press, on_release=on_release) as listener:
         listener.join()    
     
-    print("Game Over!")
-    print("Byeeee")
+    print("Game Over, Byeeee")
 
             
 except Exception as e:
